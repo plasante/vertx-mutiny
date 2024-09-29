75be8d902513d39afc063548251d9b616e833ebf
Pierre Lasante <pierre.lasante@videotron.ca>
2024-09-29 13:12:45 -0400

    First commit

---
+++ src/main/resources/db/setup.sql
@@ -0,0 +1,12 @@
+CREATE TABLE users
+(
+  id   SERIAL PRIMARY KEY,
+  name VARCHAR
+);
+
+insert into users(id, name) values (1, 'Alice');
+insert into users(id, name) values (2, 'Bob');
+insert into users(id, name) values (3, 'Charlie');
+insert into users(id, name) values (4, 'Pierre');
+insert into users(id, name) values (5, 'Karo');
+insert into users(id, name) values (6, 'Juby');
